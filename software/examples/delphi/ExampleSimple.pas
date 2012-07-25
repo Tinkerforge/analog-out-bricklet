7f3055a2386ce6d78a87d8fa0d57ca9f09073cd2
Matthias Bolte <matthias@tinkerforge.com>
2012-07-25 13:29:53 +0200

    Add Delphi example

---
+++ software/examples/delphi/ExampleSimple.pas
@@ -0,0 +1,50 @@
+program ExampleSimple;
+
+{$ifdef MSWINDOWS}{$apptype CONSOLE}{$endif}
+{$ifdef FPC}{$mode OBJFPC}{$H+}{$endif}
+
+uses
+  SysUtils, IPConnection, BrickletAnalogOut;
+
+type
+  TExample = class
+  private
+    ipcon: TIPConnection;
+    ao: TBrickletAnalogOut;
+  public
+    procedure Execute;
+  end;
+
+const
+  HOST = 'localhost';
+  PORT = 4223;
+  UID = '6Q6';
+
+var
+  e: TExample;
+
+procedure TExample.Execute;
+begin
+  { Create IP connection to brickd }
+  ipcon := TIPConnection.Create(HOST, PORT);
+
+  { Create device object }
+  ao := TBrickletAnalogOut.Create(UID);
+
+  { Add device to IP connection }
+  ipcon.AddDevice(ao);
+  { Don't use device before it is added to a connection }
+
+  { Set a voltage of 3.3V }
+  ao.SetVoltage(3300);
+
+  WriteLn('Press key to exit');
+  ReadLn;
+  ipcon.Destroy;
+end;
+
+begin
+  e := TExample.Create;
+  e.Execute;
+  e.Destroy;
+end.
