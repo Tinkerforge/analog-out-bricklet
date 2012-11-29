a5de0bf3bf1f10e6faa3d86397a9cb54da6493be
Olaf Lüke <olaf@tinkerforge.com>
2012-11-29 12:52:08 +0100

    Make C examples Protocol 2.0 compatible

--- software/examples/c/example_simple.c
+++ software/examples/c/example_simple.c
@@ -8,23 +8,20 @@
 #define UID "XYZ" // Change to your UID
 
 int main() {
-	// Create IP connection to brickd
+	// Create IP connection
 	IPConnection ipcon;
-	if(ipcon_create(&ipcon, HOST, PORT) < 0) {
-		fprintf(stderr, "Could not create connection\n");
-		exit(1);
-	}
+	ipcon_create(&ipcon);
 
 	// Create device object
 	AnalogOut ao;
-	analog_out_create(&ao, UID); 
+	analog_out_create(&ao, UID, &ipcon); 
 
-	// Add device to IP connection
-	if(ipcon_add_device(&ipcon, &ao) < 0) {
-		fprintf(stderr, "Could not connect to Bricklet\n");
+	// Connect to brickd
+	if(ipcon_connect(&ipcon, HOST, PORT) < 0) {
+		fprintf(stderr, "Could not connect\n");
 		exit(1);
 	}
-	// Don't use device before it is added to a connection
+	// Don't use device before ipcon is connected
 
 	// Set a voltage of 3.3V
 	analog_out_set_voltage(&ao, 3300);
