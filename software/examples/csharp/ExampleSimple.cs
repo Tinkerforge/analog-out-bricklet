8985d03bc288b30cd96219496bdfc0b5d2cb7239
Olaf Lüke <olaf@tinkerforge.com>
2012-12-05 15:54:40 +0100

    Add protocol 2.0 compatability to C# bindings

--- software/examples/csharp/ExampleSimple.cs
+++ software/examples/csharp/ExampleSimple.cs
@@ -8,16 +8,16 @@ class Example
 
 	static void Main() 
 	{
-		IPConnection ipcon = new IPConnection(HOST, PORT); // Create connection to brickd
-		BrickletAnalogOut ao = new BrickletAnalogOut(UID); // Create device object
-		ipcon.AddDevice(ao); // Add device to IP connection
-		// Don't use device before it is added to a connection
+		IPConnection ipcon = new IPConnection(); // Create IP connection
+		BrickletAnalogOut ao = new BrickletAnalogOut(UID, ipcon); // Create device object
+
+		ipcon.Connect(HOST, PORT); // Connect to brickd
+		// Don't use device before ipcon is connected
 
 		// Set output voltage to 3.3V
 		ao.SetVoltage(3300);
 
 		System.Console.WriteLine("Press key to exit");
 		System.Console.ReadKey();
-		ipcon.Destroy();
 	}
 }
