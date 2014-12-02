a62ae2293569c9a342df959cd7aaef2566b650f0
Olaf Lüke <olaf@tinkerforge.com>
2014-12-02 17:06:42 +0100

    Use ReadLine instead of ReadKey for C# example

--- software/examples/csharp/ExampleSimple.cs
+++ software/examples/csharp/ExampleSimple.cs
@@ -17,8 +17,8 @@ class Example
 		// Set output voltage to 3.3V
 		ao.SetVoltage(3300);
 
-		System.Console.WriteLine("Press key to exit");
-		System.Console.ReadKey();
+		System.Console.WriteLine("Press enter to exit");
+		System.Console.ReadLine();
 		ipcon.Disconnect();
 	}
 }
