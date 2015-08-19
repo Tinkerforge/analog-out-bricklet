54f0a9dc3af4ba2ab6f0bc2f066c8b5e960e5bf6
Matthias Bolte <matthias@tinkerforge.com>
2015-08-19 15:38:31 +0200

    Cleanup Perl example

--- software/examples/perl/example_simple.pl
+++ software/examples/perl/example_simple.pl
@@ -1,11 +1,11 @@
-#!/usr/bin/perl  
+#!/usr/bin/perl
 
 use Tinkerforge::IPConnection;
 use Tinkerforge::BrickletAnalogOut;
 
 use constant HOST => 'localhost';
 use constant PORT => 4223;
-use constant UID => 'ikE'; # Change to your UID
+use constant UID => 'XYZ'; # Change to your UID
 
 my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
 my $ao = Tinkerforge::BrickletAnalogOut->new(&UID, $ipcon); # Create device object
@@ -19,4 +19,3 @@ $ao->set_voltage(3300);
 print "Press any key to exit...\n";
 <STDIN>;
 $ipcon->disconnect();
-
