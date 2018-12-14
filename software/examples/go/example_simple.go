92b5e3a5de9ccdf3a39fb6ce19af5034e118a950
Erik Fleckstein <erik@tinkerforge.com>
2018-12-14 16:07:03 +0100

    Add go examples.

---
+++ software/examples/go/example_simple.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"tinkerforge/analog_out_bricklet"
+	"tinkerforge/ipconnection"
+)
+
+const ADDR string = "localhost:4223"
+const UID string = "XYZ" // Change XYZ to the UID of your Analog Out Bricklet.
+
+func main() {
+	ipcon := ipconnection.New()
+	defer ipcon.Close()
+	ao, _ := analog_out_bricklet.New(UID, &ipcon) // Create device object.
+
+	ipcon.Connect(ADDR) // Connect to brickd.
+	defer ipcon.Disconnect()
+	// Don't use device before ipcon is connected.
+
+	// Set output voltage to 3.3V
+	ao.SetVoltage(3300)
+
+	fmt.Print("Press enter to exit.")
+	fmt.Scanln()
+
+}
